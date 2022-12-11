9c57925a18e2de74751123f80332030d421afa12
kaleab <kaleababrham200@gmail.com>
2022-12-11 03:43:09 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given phrase
+ * Return: 0 if exited exactly, non-zero otherwise
+ */
+
+int main(void)
+{
+       puts("\"programming is like building a multilingual puzzle");	
+       return (0);
+}        
